a8496d326ef21aaaf8053a47f31e2f49d5fcea9c
Derek <stiau334@gmail.com>
2018-07-13 05:29:08 +0800

    Initial Commit

---
+++ 肉寶/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  肉寶
+//
+//  Created by Derek on 2018/7/13.
+//  Copyright © 2018年 Derek. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
